de5daf0df57a13be01f5901c1f2df158ebffe974
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2019-12-18 01:38:18 +1300

    Fix pest grammar a bit, and add more to parse function.

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,12 @@ use std::path::Path;
 #[grammar = "jumpchain.pest"]
 struct JumpchainParser;
 
+#[derive(Debug)]
 struct Jumpchain<'a> {
   sections: Vec<Section<'a>>,
 }
 
+#[derive(Debug)]
 struct Section<'a> {
   name:             &'a str,
   points_increment: i64,
@@ -30,11 +32,28 @@ struct Section<'a> {
   points_remainder: Option<i64>,
 }
 
+use std::fs;
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if(args.len() == 0) {
+    if(args.len() == 1) {
       println!("Usage: jumpchain [file]")
     } else {
+      let unparsed_file = fs::read_to_string(args[1].clone()).expect("cannot read file");
+      let jumpchain: Jumpchain = parse_jumpchain_file(&unparsed_file).expect("unsuccessful parse");
+      println!("{:?}", jumpchain);
     }
     Ok(())
+}
+
+fn parse_jumpchain_file<'a>(file: &str) -> Result<Jumpchain<'a>, pest::error::Error<Rule>> {
+    let jumpchain_stream = JumpchainParser::parse(Rule::document, file)?.next().unwrap();
+    println!("{:?}", jumpchain_stream);
+    // Make section
+    use pest::iterators::Pair;
+    fn make_section<'a>(section_pair: Pair<Rule>) -> Section<'a> {
+      let mut section = Section{name: "", points_increment: 0, jump_type: "", jump_type_cost: 0, perk: vec!{}, remainder: None, points_spent: None, points_remainder: None};
+      section
+    }
+    let sections = jumpchain_stream.into_inner().map(|section| make_section(section)).collect();
+    Ok(Jumpchain{sections: sections})
 }
