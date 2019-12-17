cccd78d8585c7a03c46ce035f55df1c83f80cf12
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2019-12-17 23:53:33 +1300

    Start to build up data structures.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,20 @@ use pest::Parser;
 #[grammar = "jumpchain.pest"]
 struct JumpchainParser;
 
+struct Jumpchain<'a> {
+  sections: Vec<Section<'a>>,
+}
+
+struct Section<'a> {
+  name:             &'a str,
+  points_increment: i64,
+  jump_type:        &'a str,
+  perk:             Vec<(&'a str, i64)>,
+  remainder:        Option<i64>,
+  points_spent:     Option<i64>,
+  points_remainder: Option<i64>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
