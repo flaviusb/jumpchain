fe63e3b23492d497915389ad692c3d2e639dcda6
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2019-12-18 00:36:02 +1300

    Add more cost/refund positions.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ extern crate pest_derive;
 
 use pest::Parser;
 
+use std::env;
+use std::option;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 #[derive(Parser)]
 #[grammar = "jumpchain.pest"]
 struct JumpchainParser;
@@ -16,12 +23,18 @@ struct Section<'a> {
   name:             &'a str,
   points_increment: i64,
   jump_type:        &'a str,
+  jump_type_cost:   i64,
   perk:             Vec<(&'a str, i64)>,
   remainder:        Option<i64>,
   points_spent:     Option<i64>,
   points_remainder: Option<i64>,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if(args.len() == 0) {
+      println!("Usage: jumpchain [file]")
+    } else {
+    }
+    Ok(())
 }
