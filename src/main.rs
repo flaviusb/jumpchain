482bb3d5bfaecf128741f61bd24ae9e45641b0a8
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2019-12-18 20:06:07 +1300

    Add parse_cost_refund.

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ struct Jumpchain<'a> {
 struct Section<'a> {
   name:             &'a str,
   points_increment: i64,
-  jump_type:        &'a str,
+  jump_type:        Vec<(&'a str, &'a str, i64)>,
   jump_type_cost:   i64,
   perk:             Vec<(&'a str, i64)>,
   remainder:        Option<i64>,
@@ -45,18 +45,37 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+// Cost is positive, refund is negative, Free is zero
+use pest::iterators::Pair;
+fn parse_cost_refund(cost_refund: Pair<Rule>) -> i64 {
+  match cost_refund.as_rule() {
+    Rule::refund => (0 - cost_refund.into_inner().next().unwrap().as_str().parse::<i64>().unwrap()),
+    Rule::cost   => {
+      match &cost_refund.into_inner().collect::<Vec<Pair<Rule>>>()[..] {
+        []     => 0,
+        [a]    => a.as_str().parse::<i64>().unwrap(),
+        [a, b] => b.as_str().parse::<i64>().unwrap(),
+        _      => panic!("Too many things."),
+      }
+    }
+    _            => panic!("parse_cost_refund given {:}", cost_refund),
+  }
+}
+
 fn parse_jumpchain_file<'a>(file: &'a str) -> Result<Jumpchain<'a>, pest::error::Error<Rule>> {
     let jumpchain_stream = JumpchainParser::parse(Rule::document, file)?;
     println!("Jumpchain stream: {:?}", jumpchain_stream);
     // Make section
     use pest::iterators::Pair;
     fn make_section<'a>(section_pair: Pair<'a, Rule>) -> Section<'a> {
-      let mut section = Section{name: "", points_increment: 0, jump_type: "", jump_type_cost: 0, perk: vec!{}, remainder: None, points_spent: None, points_remainder: None};
+      let mut section = Section{name: "", points_increment: 0, jump_type: vec!{}, jump_type_cost: 0, perk: vec!{}, remainder: None, points_spent: None, points_remainder: None};
       //println!("section_pair.into_inner().next().unwrap().into_inner(): {:?}",section_pair.into_inner().next().unwrap().into_inner());
       let mut section_iterator = section_pair.into_inner();
       section.name = section_iterator.next().unwrap().into_inner().as_str();
       let intermediate = (section_iterator.next().unwrap().into_inner().next().unwrap().as_str());
       section.points_increment = intermediate.parse::<i64>().unwrap();
+      // Get all jump type data
+      let jump_types_stream = section_iterator.next().unwrap().into_inner();
       section
     }
     let sections = jumpchain_stream.filter(|pair| match pair.as_rule() {
